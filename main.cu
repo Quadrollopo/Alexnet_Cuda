f83103c874ee826e497123201c887d4fb63a948a
Quadrollopo <davide97@gmail.com>
2022-02-03 11:26:13 +0100

    Remove class fullLayer

--- main.cu
+++ main.cu
@@ -1,28 +1,20 @@
 #include <iostream>
 #include <vector>
 #include "network.h"
-//#include "matrixMul.cuh"
+#include "matrixMul.cuh"
 using namespace std;
 
-
-class fullLayer{
-public:
-	fullLayer(int num, fullLayer* next);
-	int num_neurons;
-	float* neurons;
-	float** weigths;
-	fullLayer* nextLayer;
-	void forward();
-};
 
 int main() {
 
 	Network net(5);
 	net.addFullLayer(4);
 	net.addFullLayer(3);
-	float in[] = {3.0f, 2.0f, 2.0f, 2.0f, 2.0f};
+	float in[] = {3.0f, 2.0f, 2.5f, 2.0f, 2.0f};
 	float* out = net.forward(in);
-	cout << out;
+	for (int i = 0; i < 3; i++)
+		cout << out[i] << endl;
+	matrixMul<<<2,2>>>();
 	return 0;
 	std::vector<float*> x;
 	float y[10];
