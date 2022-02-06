af87a8553b1c26cbc9fc2c2b55a939b359e1ff4c
Quadrollopo <davide97@gmail.com>
2022-02-06 16:58:42 +0100

    Add utils library

---
+++ utils.cuh
@@ -0,0 +1,25 @@
+#ifndef ALEXNET_UTLIS_H
+#define ALEXNET_UTLIS_H
+
+#endif //ALEXNET_UTLIS_H
+
+static float reLU(float f){
+	return f > 0.0f ? f : 0.0f;
+}
+
+static float Heaviside(float f){
+	return f > 0.0f ? 1.0f : 0.0f;
+}
+
+static float sigmoid(float f){
+	return 1.f/ (1.f + exp(-f));
+}
+
+static float der_sigmoid(float f) {
+	return f * (1 - f);
+}
+
+static double sech2(float x) {
+	float sh = 1.0f / std::cosh(x);   // sech(x) == 1/cosh(x)
+	return sh*sh;                     // sech^2(x)
+}
