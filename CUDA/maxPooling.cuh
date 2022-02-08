0318ffc1fc0d75fe2de905d42f49b136bc8a8c4b
Quadrollopo <davide97@gmail.com>
2022-02-08 16:37:49 +0100

    Refactor + max_pooling cpu

--- CUDA/maxPooling.cuh
+++ CUDA/maxPooling.cuh
@@ -3,6 +3,6 @@
 
 __global__ void max_pooling_CUDA(float *image, float *kernel, float *res, int image_size, int kernel_size, int stride, int pad, int res_dim);
 float* max_pooling(float *image, float *kernel, int image_size, int kernel_size, int stride, int pad);
-float* max_pooling_CPU(float *image, float *kernel, int kern_size, int img_size, int stride, bool pad);
+float* max_pooling_CPU(float *image, int pool_size, int img_size, int stride);
 
 #endif
